c3b6669c517d7d87309af8171c5a540fdd65618b
Aditya S Khanna <khanna7@uw.edu>
2018-07-20 08:51:42 -0500

    examples of fits that gave degenerate models with distance term

--- fit-ergms/population_2014-08-17--11.00.43--analysis.R
+++ fit-ergms/population_2014-08-17--11.00.43--analysis.R
@@ -7,10 +7,11 @@ rm(list=ls())
 library(network)
 library(ergm)
 library(dplyr)
+library(ergm.userterms)
 
 # Data ----------
 
-data <- read.csv("../data/population_2014-08-17--11.00.43.csv", header = T)
+data <- read.csv("/project/khanna7/Projects/midway2/HepCep/data/pwids_with_lat_lon.csv", header = T)
 dim(data)
 glimpse(data)
 
@@ -37,43 +38,109 @@ mdeg <- nedges/nrow(data)
 sum(indeg)
 sum(outdeg)
 
+dist.prop.distribution <- c(30, 12, 12, 15, 15, 10)
+dist.nedge.distribution <- nedges*dist.prop.distribution
+
+    
 # Fit ERGM ----------
 
 n0 <- network.initialize(n = nrow(data))
+n0 %v% "lat" <- data$lat
+n0 %v% "lon" <- data$lon
 
-fit <- ergm(n0 ~ edges + odegree(0) + idegree(0),
-            target.stats = c(nedges, outdeg_tbl[1], indeg_tbl[1]),
+dist.terms <- 1:4
+
+##  fit <- ergm(n0 ~ edges + odegree(0) + idegree(0),
+##             target.stats = c(nedges, outdeg_tbl[1], indeg_tbl[1]),
+##             eval.loglik = FALSE,
+##             control = control.ergm(MCMLE.maxit = 100)
+##             )
+
+## sim <- simulate(fit)
+## sim
+
+## fit.add.degrees <-
+##   ergm(
+##     n0 ~ edges + odegree(c(0, 2)) + idegree(c(0,2)),
+##     target.stats = c(nedges, outdeg_tbl[c(1,3)], indeg_tbl[c(1,3)]),
+##     eval.loglik = FALSE,
+##     control = control.ergm(MCMLE.maxit = 100)
+##   )
+
+## summary(fit.add.degrees)
+## sim.fit.add.degrees <- simulate(fit.add.degrees)
+
+## fit <- ergm(n0 ~ edges + odegree(0) + idegree(0)+
+##            + dist(1:6),
+##             target.stats = c(nedges, outdeg_tbl[1], indeg_tbl[1],
+##                              dist.nedge.distribution),
+##             eval.loglik = FALSE,
+##             control = control.ergm(MCMLE.maxit = 500)
+##             )
+
+##############################
+#ABOVE ERGM DID NOT CONVERGE
+##############################
+
+## fit <- ergm(n0 ~ edges + odegree(0) + idegree(0)+
+##            + dist(dist.terms),
+##             target.stats = c(nedges, outdeg_tbl[1], indeg_tbl[1],
+##                              dist.nedge.distribution[dist.terms]),
+##             eval.loglik = FALSE,
+##             control = control.ergm(MCMLE.maxit = 500)
+##             )
+##############################
+#ABOVE ERGM DID NOT CONVERGE
+##############################
+
+fit <- ergm(n0 ~ edges + 
+            dist(dist.terms),
+            target.stats = c(nedges, 
+                             dist.nedge.distribution[dist.terms]),
             eval.loglik = FALSE,
-            control = control.ergm(MCMLE.maxit = 100)
+            control = control.ergm(MCMLE.maxit = 500)
             )
+
 
 sim <- simulate(fit)
 sim
 
-fit.add.degrees <-
-  ergm(
-    n0 ~ edges + odegree(c(0, 2)) + idegree(c(0,2)),
-    target.stats = c(nedges, outdeg_tbl[c(1,3)], indeg_tbl[c(1,3)]),
-    eval.loglik = FALSE,
-    control = control.ergm(MCMLE.maxit = 100)
-  )
+## fit.add.degrees.dist <-
+##   ergm(
+##     n0 ~ edges + odegree(c(0, 2)) + idegree(c(0,2))+
+##     dist(1:6),
+##     target.stats = c(nedges, outdeg_tbl[c(1,3)], indeg_tbl[c(1,3)],
+##                      dist.nedge.distribution),
+##     eval.loglik = FALSE,
+##     control = control.ergm(MCMLE.maxit = 100)
+##   )
 
-summary(fit.add.degrees)
-sim.fit.add.degrees <- simulate(fit.add.degrees)
-            
+## summary(fit.add.degrees.dist)
+## sim.fit.add.degrees.dist <- simulate(fit.add.degrees.dist)
+
 # Compare simulated out- and in-degrees vs empirical
 
-# only degree 0
-summary(sim ~ odegree(0:3))/sum(summary(sim ~ odegree(0:3))) 
-outdeg_tbl/nrow(data)
+## # only degree 0
+## summary(sim ~ odegree(0:3))/sum(summary(sim ~ odegree(0:3))) 
+## outdeg_tbl/nrow(data)
 
-summary(sim ~ idegree(0:3))/sum(summary(sim ~ idegree(0:3)))
-indeg_tbl/nrow(data)
+## summary(sim ~ idegree(0:3))/sum(summary(sim ~ idegree(0:3)))
+## indeg_tbl/nrow(data)
 
-#degrees 0 and 2
+## #degrees 0 and 2
 
-summary(sim.fit.add.degrees ~ odegree(0:3))/sum(summary(sim.fit.add.degrees ~ odegree(0:3))) 
-outdeg_tbl[1:4]/nrow(data)
+## summary(sim.fit.add.degrees ~ odegree(0:3))/sum(summary(sim.fit.add.degrees ~ odegree(0:3))) 
+## outdeg_tbl[1:4]/nrow(data)
 
-summary(sim.fit.add.degrees ~ idegree(0:3))/sum(summary(sim.fit.add.degrees ~ idegree(0:3)))
-indeg_tbl[1:4]/nrow(data)
+## summary(sim.fit.add.degrees ~ idegree(0:3))/sum(summary(sim.fit.add.degrees ~ idegree(0:3)))
+## indeg_tbl[1:4]/nrow(data)
+
+#degrees 0 and 2 and dist
+
+## summary(sim.fit.add.degrees.dist ~ odegree(0:3))/sum(summary(sim.fit.add.degrees.dist ~ odegree(0:3))) 
+## outdeg_tbl[1:4]/nrow(data)
+
+## summary(sim.fit.add.degrees.dist ~ idegree(0:3))/sum(summary(sim.fit.add.degrees.dist ~ idegree(0:3)))
+## indeg_tbl[1:4]/nrow(data)
+
+save.image("large-net-dist-term.RData")
