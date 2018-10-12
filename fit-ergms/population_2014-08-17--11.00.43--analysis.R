0b880be0605fa627cbafc981c603d106d11b4999
Aditya S Khanna <khanna7@uw.edu>
2018-10-12 16:17:30 -0500

    add other attributes to the initialized network and fitted ergm

--- fit-ergms/population_2014-08-17--11.00.43--analysis.R
+++ fit-ergms/population_2014-08-17--11.00.43--analysis.R
@@ -15,7 +15,6 @@ data <- read.csv("/project/khanna7/Projects/midway2/HepCep/data/pwids_with_lat_l
 dim(data)
 glimpse(data)
 
-
 # Summary Statistics ----------
 
 indeg <- data$Drug_in_degree
@@ -41,20 +40,45 @@ sum(outdeg)
 dist.prop.distribution <- c(30, 12, 12, 15, 15, 10, 6)/100 #seventh term calculated as 1-sum(of first 6)
 dist.nedge.distribution <- nedges*dist.prop.distribution
 
-# Fit ERGM ----------
+# Recode variables ----------
+
+## chicago vs nonchicago
+data <-  
+  mutate(data, chicago = ifelse(substr(Zip, 1, 3) == "606", 1, 0))
+
+
+# Initialize network and add attributes ----------
 
 n0 <- network.initialize(n = nrow(data))
+
+n0 %v% "age" <- data$Age
+n0 %v% "gender" <- as.numeric(data$Gender) #1=F; 2=M
+n0 %v% "race" <- as.numeric(data$Race)# 1=Hisp, 2=NH Black,3=NHWhite, 4=O
+n0 %v% "zip" <- data$Zip
+n0 %v% "syringe_source" <- as.numeric(data$Syringe_source) #1=HR, 2=nonHR
+n0 %v% "hcv" <- as.numeric(data$HCV)#1=C, 2=E, 3=inf.acute, 4=R, 5=S
+n0 %v% "age_started" <- data$Age_Started
+n0 %v% "num_buddies" <- data$Num.Buddies
+n0 %v% "daily_injection_intensity" <- data$Daily_injection_intensity
+n0 %v% "fraction_recept_sharing" <- data$Fraction_recept_sharing
+n0 %v% "zz" <- data$zz
+n0 %v% "azi" <- data$azi
+#n0 %v% "zip1" <- data$Zip.1 #identical to "Zip"
 n0 %v% "lat" <- data$lat
 n0 %v% "lon" <- data$lon
+n0 %v% "chicago" <- data$chicago #mutated chicago variable
 
 dist.terms <- c(1:5,7) #leave one out 
 
+
+# Fit ERGM ----------
+
 fit <- ergm(n0 ~ edges + 
             dist(dist.terms) +
-            odegree(c(0, 2)) + idegree(c(0,2)),
+            odegree(c(0,2,3)) + idegree(c(0,2,3)),
             target.stats = c(nedges, 
                              dist.nedge.distribution[dist.terms],
-                             outdeg_tbl[c(1,3)], indeg_tbl[c(1,3)]),
+                             outdeg_tbl[c(1,3,4)], indeg_tbl[c(1,3,4)]),
             eval.loglik = FALSE,
             control = control.ergm(MCMLE.maxit = 500)
             )
@@ -62,43 +86,5 @@ fit <- ergm(n0 ~ edges +
 
 sim <- simulate(fit)
 sim
-
-## fit.add.degrees.dist <-
-##   ergm(
-##     n0 ~ edges + odegree(c(0, 2)) + idegree(c(0,2))+
-##     dist(1:6),
-##     target.stats = c(nedges, outdeg_tbl[c(1,3)], indeg_tbl[c(1,3)],
-##                      dist.nedge.distribution),
-##     eval.loglik = FALSE,
-##     control = control.ergm(MCMLE.maxit = 100)
-##   )
-
-## summary(fit.add.degrees.dist)
-## sim.fit.add.degrees.dist <- simulate(fit.add.degrees.dist)
-
-# Compare simulated out- and in-degrees vs empirical
-
-## # only degree 0
-## summary(sim ~ odegree(0:3))/sum(summary(sim ~ odegree(0:3))) 
-## outdeg_tbl/nrow(data)
-
-## summary(sim ~ idegree(0:3))/sum(summary(sim ~ idegree(0:3)))
-## indeg_tbl/nrow(data)
-
-## #degrees 0 and 2
-
-## summary(sim.fit.add.degrees ~ odegree(0:3))/sum(summary(sim.fit.add.degrees ~ odegree(0:3))) 
-## outdeg_tbl[1:4]/nrow(data)
-
-## summary(sim.fit.add.degrees ~ idegree(0:3))/sum(summary(sim.fit.add.degrees ~ idegree(0:3)))
-## indeg_tbl[1:4]/nrow(data)
-
-#degrees 0 and 2 and dist
-
-## summary(sim.fit.add.degrees.dist ~ odegree(0:3))/sum(summary(sim.fit.add.degrees.dist ~ odegree(0:3))) 
-## outdeg_tbl[1:4]/nrow(data)
-
-## summary(sim.fit.add.degrees.dist ~ idegree(0:3))/sum(summary(sim.fit.add.degrees.dist ~ idegree(0:3)))
-## indeg_tbl[1:4]/nrow(data)
 
 save.image("large-net-dist-term.RData")
