5431c1529abb7fbc33fa91bd2556107017fc26d6
Aditya S Khanna <khanna7@uw.edu>
2019-05-20 12:55:12 -0500

    ergm fit with gender mixing meta data

--- fit-ergms/ergm-estimation-with-meta-data.R
+++ fit-ergms/ergm-estimation-with-meta-data.R
@@ -3,6 +3,7 @@
 
 rm(list=ls())
 
+
 # Libraries ----------
 
 library(network)
@@ -10,98 +11,99 @@ library(ergm)
 library(dplyr)
 library(ergm.userterms)
 
+
 # Data ----------
 
-load("large-net-dist-term.RData") 
-# refer to "population_2014-08-17--11.00.43--analysis.R" for details
-
+load("meta-mixing-init-net.RData")
+# refer to "population_2014-08-17--11.00.43--analysis.R" for an example
 
 # Recode to add new variables to dataset ----------
 
-# young (< 26, coded 1) vs old (>= 26, coded 0) (from SATHCAP)
-data <-  
-  mutate(data, young = ifelse(Age < 26, 1, 0))
+# young (< 26, to be set = 1) vs old (>= 26, to be set = 0) 
+age.cat <- n0 %v% "age.cat"
+age.cat.df <- as.data.frame(age.cat)
+age.cat.df <-  
+  mutate(age.cat, young = ifelse(age.cat == 0, 1, 0), .data = age.cat.df)
+xtabs(~age.cat+young, data = age.cat.df)
 
 
 # Initialize network and add attributes ----------
 
-n0 %v% "young" <- data$young
+n0 %v% "young" <- age.cat.df$young
 
 
-# Generate target statistics from SATHCAP mixing data ----------
+# Generate target statistics from meta-mixing data ----------
 
 ## gender 
 ### mixing information from meta-analysis of sathcap AND socnet
-male.pctmale <- 0.53
+edges.male.end <- mean(c(0.58, 0.59))
+edges.female.end <- mean(c(0.40, 0.41))
+
+male.pctmale <- 0.53 
 male.pctfemale <- 0.47
 female.pctmale <- 0.75
 female.pctfemale <- 0.22
 
-### mixing from simulation
-gender.mm <- mixingmatrix(sim, "gender") #fem=1, male=2
-from.female <- sum(gender.mm$matrix[,1])
-from.male <- sum(gender.mm$matrix[,2])
+### set gender targets 
+tgt.male.pctmale <- edges_target*edges.male.end*male.pctmale
+tgt.male.pctfemale <- edges_target*edges.male.end*male.pctfemale
+tgt.female.pctmale <- edges_target*edges.female.end*female.pctmale  
+tgt.female.pctfemale <- edges_target*edges.female.end*female.pctfemale
 
-### set gender targets from sathcap
-tgt.male.pctmale <- from.male*male.pctmale
-tgt.male.pctfemale <- from.male*male.pctfemale
-tgt.female.pctmale <- from.female*male.pctmale  
-tgt.female.pctfemale <- from.female*male.pctfemale
-
-## young (1=young; 0=old) 
-### mixing information from meta-analysis of sathcap AND socnet
-young.pctyoung <- 0.60
-young.pctold <- 0.40
-old.pctyoung <- 0.14
-old.pctold <- 0.86
-
-### mixing from simulation
-sim %v% "young" <- n0 %v% "young" #attach "young" att for sim from n0
-young.mm <- mixingmatrix(sim, "young")
-from.old <- sum(young.mm$matrix[,1])
-from.young <- sum(young.mm$matrix[,2])
-
-### set young targets from sathcap
-tgt.young.pctyoung <- from.young*young.pctyoung
-tgt.young.pctold <- from.young*young.pctold
-tgt.old.pctyoung <- from.old*young.pctyoung  
-tgt.old.pctold <- from.old*young.pctold
-
-## chicago 
-### mixing information from meta-analysis of sathcap AND socnet
-chicago.pctchicago <- 0.67
-chicago.pctnonchicago <- 0.30
-nonchicago.pctchicago <- 0.60
-nonchicago.pctnonchicago <- 0.40
-
-### mixing from simulation
-chicago.mm <- mixingmatrix(sim, "chicago") #fem=1, chicago=2
-from.nonchicago <- sum(chicago.mm$matrix[,1])
-from.chicago <- sum(chicago.mm$matrix[,2])
-
-### set chicago targets from sathcap
-tgt.chicago.pctchicago <- from.chicago*chicago.pctchicago
-tgt.chicago.pctnonchicago <- from.chicago*chicago.pctnonchicago
-tgt.nonchicago.pctchicago <- from.nonchicago*chicago.pctchicago
-tgt.nonchicago.pctnonchicago <- from.nonchicago*chicago.pctnonchicago
-
-## race (1=Hisp, 2=NH Black,3=NHWhite, 4=O)
-race.1.1 <- 0.58
-race.1.2 <- 0.12
-race.1.3 <- 0.18
-race.1.4 <- 0.12
-race.2.1 <- 0.06
-race.2.2 <- 0.83
-race.2.3 <- 0.10
-race.2.4 <- 0.01
-race.3.1 <- 0.12
-race.3.2 <- 0.10
-race.3.3 <- 0.73
-race.3.4 <- 0.05
-race.4.1 <- 0.22
-race.4.2 <- 0.21
-race.4.3 <- 0.43
-race.4.4 <- 0.14
+# ## young (1=young; 0=old) 
+# ### mixing information from meta-analysis of sathcap AND socnet
+# young.pctyoung <- 0.60
+# young.pctold <- 0.40
+# old.pctyoung <- 0.14
+# old.pctold <- 0.86
+# 
+# ### mixing from simulation
+# sim %v% "young" <- n0 %v% "young" #attach "young" att for sim from n0
+# young.mm <- mixingmatrix(sim, "young")
+# from.old <- sum(young.mm$matrix[,1])
+# from.young <- sum(young.mm$matrix[,2])
+# 
+# ### set young targets from sathcap
+# tgt.young.pctyoung <- from.young*young.pctyoung
+# tgt.young.pctold <- from.young*young.pctold
+# tgt.old.pctyoung <- from.old*young.pctyoung  
+# tgt.old.pctold <- from.old*young.pctold
+# 
+# ## chicago 
+# ### mixing information from meta-analysis of sathcap AND socnet
+# chicago.pctchicago <- 0.67
+# chicago.pctnonchicago <- 0.30
+# nonchicago.pctchicago <- 0.60
+# nonchicago.pctnonchicago <- 0.40
+# 
+# ### mixing from simulation
+# chicago.mm <- mixingmatrix(sim, "chicago") #fem=1, chicago=2
+# from.nonchicago <- sum(chicago.mm$matrix[,1])
+# from.chicago <- sum(chicago.mm$matrix[,2])
+# 
+# ### set chicago targets from sathcap
+# tgt.chicago.pctchicago <- from.chicago*chicago.pctchicago
+# tgt.chicago.pctnonchicago <- from.chicago*chicago.pctnonchicago
+# tgt.nonchicago.pctchicago <- from.nonchicago*chicago.pctchicago
+# tgt.nonchicago.pctnonchicago <- from.nonchicago*chicago.pctnonchicago
+# 
+# ## race (1=Hisp, 2=NH Black,3=NHWhite, 4=O)
+# race.1.1 <- 0.58
+# race.1.2 <- 0.12
+# race.1.3 <- 0.18
+# race.1.4 <- 0.12
+# race.2.1 <- 0.06
+# race.2.2 <- 0.83
+# race.2.3 <- 0.10
+# race.2.4 <- 0.01
+# race.3.1 <- 0.12
+# race.3.2 <- 0.10
+# race.3.3 <- 0.73
+# race.3.4 <- 0.05
+# race.4.1 <- 0.22
+# race.4.2 <- 0.21
+# race.4.3 <- 0.43
+# race.4.4 <- 0.14
 
 # Fit ERGM (with SATHCAP mixing) ----------
 
@@ -110,33 +112,13 @@ fit.sathcap.mixing <-
     n0 ~ 
       edges +
       #odegree(c(0, 2, 3)) + idegree(c(0, 2, 3))+
-      nodematch("gender", diff=TRUE)+
-      nodematch("young", diff=TRUE)+
-      nodematch("chicago", diff=TRUE)+
-      nodematch("race", diff=T),
-      #+
-      #nodeofactor("race", base=1)+
-      #nodeifactor("race", base=1),
-    target.stats = c(nedges,
-                     #outdeg_tbl[c(1, 3, 4)], indeg_tbl[c(1, 3, 4)],
-                     c(tgt.female.pctfemale, tgt.male.pctmale),
-                     c(tgt.young.pctyoung, tgt.old.pctold),
-                     c(tgt.chicago.pctchicago, tgt.nonchicago.pctnonchicago),
-                     c(race.1.1,           
-                                 race.2.2, 
-                                           race.3.3, 
-                                                     race.4.4
-                     )
-                     #c(sum(race.2.1, race.2.2, race.2.3, race.2.4),
-                        #sum(race.3.1, race.3.2, race.3.3, race.3.4),
-                        #sum(race.4.1, race.4.2, race.4.3, race.4.4)),
-                     #c(sum(race.1.2, race.2.2, race.3.2, race.4.2),
-                        #sum(race.1.3, race.2.3, race.3.3, race.4.3),
-                        #sum(race.1.4, race.2.4, race.3.4, race.4.4))
+      nodemix("gender", base=1),
+      target.stats = c(edges_target,
+                     c(tgt.female.pctmale, tgt.male.pctfemale, tgt.male.pctmale)
                      ),
     eval.loglik = FALSE,
     control = control.ergm(MCMLE.maxit = 500)
   )
 
 
-save.image("sathcap-mixing-network.RData")
+save.image("out/meta-mixing-ergm-fit.RData")
