51f0e8af3f262b5b0d6e8edb96d2a637f3023f9a
Aditya S Khanna <khanna7@uw.edu>
2019-12-20 13:49:59 -0600

    update code to simulate networks, compute model summaries, and generate net stats

--- fit-ergms/simulate-networks-from-meta-data-ergm-fit.R
+++ fit-ergms/simulate-networks-from-meta-data-ergm-fit.R
@@ -15,7 +15,7 @@ library(dplyr)
 
 # Data ----------
 
-load("out/model3-5.RData")
+load("out/model6-2-increase-san.RData")
 
 
 # Model summary
@@ -23,7 +23,7 @@ summary(fit.metadata.mixing)
 
 
 # MCMC diagnostics
-pdf(file="out/model3-5.pdf")
+pdf(file="out/model7-increase-san-increase-san.pdf")
 mcmc.diagnostics(fit.metadata.mixing)
 dev.off()
 
@@ -39,8 +39,30 @@ net
 summary(net ~ edges)
 edges_target
 
+summary(net ~ nodemix("gender"))
+round(c(tgt.female.pctfemale, tgt.female.pctmale, tgt.male.pctfemale, tgt.male.pctmale), 0)
+
+summary(net ~ nodemix("young"))
+round(c(tgt.old.pctold, tgt.old.pctyoung, tgt.young.pctold, tgt.young.pctyoung))
+
+summary(net ~ nodemix("race.num"))
+round(
+  c(target.w.w, target.b.w, target.h.w, target.o.w,
+    target.w.b, target.b.b, target.h.b, target.o.b,
+    target.w.h, target.b.h, target.h.h, target.o.h,
+    target.w.o, target.b.o, target.h.o, target.o.o),
+  0
+                 )
+
+
+
 summary(net ~ idegree(0:4)) 
 inedges$n_nodes[1:4]
 
+summary(net ~ odegree(0:4)) 
+outedges$n_nodes[1:5]
 
-save.image("out/simulate-model-3-5.RData")
+
+save.image("out/simulate-model6-2-increase-san.RData")
+
+
