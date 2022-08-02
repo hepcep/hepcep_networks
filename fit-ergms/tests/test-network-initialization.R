4234e5843352910ab7b0165a1597e63b2bedfd4e
Aditya Khanna <khanna7.work@gmail.com>
2022-08-02 15:02:13 -0400

    test race distributions

--- fit-ergms/tests/test-network-initialization.R
+++ fit-ergms/tests/test-network-initialization.R
@@ -31,11 +31,35 @@ prop_male_in_network <- as.numeric(table(n0%v%"gender")["male"]/(network.size(n0
 expected_females <- which(demog_data$sex == "F")
 expected_females_prop <- length(expected_females)/nrow(demog_data)
 
-expect_equal(prop_fem_in_network, expected_females_prop, tolerance=0.001)
-
-
 expected_males <- which(demog_data$sex == "M")
 expected_males_prop <- length(expected_males)/nrow(demog_data)
 
+expect_equal(prop_fem_in_network, expected_females_prop, tolerance=0.001)
 expect_equal(prop_male_in_network, expected_males_prop, tolerance=0.001)
 
+
+# Test 2: race  ----------
+
+table(n0%v%"race")
+
+prop_white_in_network <- as.numeric(table(n0%v%"race")["white"]/(network.size(n0)))
+prop_black_in_network <- as.numeric(table(n0%v%"race")["black"]/(network.size(n0)))
+prop_hispanic_in_network <- as.numeric(table(n0%v%"race")["hispanic"]/(network.size(n0)))
+prop_other_in_network <- as.numeric(table(n0%v%"race")["other"]/(network.size(n0)))
+
+expected_white <- which(demog_data$race == "W")
+expected_white_prop <- length(expected_white)/nrow(demog_data)
+
+expected_black <- which(demog_data$race == "B")
+expected_black_prop <- length(expected_black)/nrow(demog_data)
+
+expected_hispanic <- which(demog_data$race == "H")
+expected_hispanic_prop <- length(expected_hispanic)/nrow(demog_data)
+
+expected_other <- which(demog_data$race == "O")
+expected_other_prop <- length(expected_other)/nrow(demog_data)
+
+expect_equal(prop_white_in_network, expected_white_prop, tolerance=0.01)
+expect_equal(prop_black_in_network, expected_black_prop, tolerance=0.01)
+expect_equal(prop_hispanic_in_network, expected_hispanic_prop, tolerance=0.01)
+expect_equal(prop_other_in_network, expected_other_prop, tolerance=0.01)
