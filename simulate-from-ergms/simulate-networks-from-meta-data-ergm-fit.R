948cd2332c5731844b173bb79dd48ac957b9470e
Aditya S Khanna <khanna7.work@gmail.com>
2022-10-03 13:08:30 -0500

    remove code to simulate 1 network (already commented out)

--- simulate-from-ergms/simulate-networks-from-meta-data-ergm-fit.R
+++ simulate-from-ergms/simulate-networks-from-meta-data-ergm-fit.R
@@ -22,43 +22,6 @@ load("../fit-ergms/out/racemix-plus-dist-plus-negbin-odeg0-3-indeg0-1-orignialda
 summary(fit.metadata.mixing)
 
 
-# Simulate 1 network ----------
-
-net <- simulate(fit.metadata.mixing)
-net
-
-
-# Investigate netstats on 1 network ----------
-
-summary(net ~ edges)
-edges_target
-
-summary(net ~ nodemix("gender"))
-round(c(tgt.female.pctfemale, tgt.female.pctmale, tgt.male.pctfemale, tgt.male.pctmale), 0)
-
-summary(net ~ nodemix("young"))
-round(c(tgt.old.pctold, tgt.old.pctyoung, tgt.young.pctold, tgt.young.pctyoung))
-
-summary(net ~ nodemix("race.num"))
-round(
-  c(target.w.w, target.b.w, target.h.w, target.o.w,
-    target.w.b, target.b.b, target.h.b, target.o.b,
-    target.w.h, target.b.h, target.h.h, target.o.h,
-    target.w.o, target.b.o, target.h.o, target.o.o),
-  0
-                 )
-
-
-
-summary(net ~ idegree(0:4)) 
-inedges$n_nodes[1:4]
-
-summary(net ~ odegree(0:4)) 
-outedges$n_nodes[1:5]
-
-summary(net ~ dist(1:4))
-round(dist.nedge.distribution)
-
 # Simulate 100 networks ----------
 
 nsim.vec <- 1:100
@@ -76,7 +39,8 @@ for (iter in 1:length(nsim.vec)){
 
 ## edgecount
 ecount <- unlist(lapply(sim_results, network.edgecount))
-summary(ecount); edges_target
+summary(ecount)
+edges_target
 
 ## outdegree
 outdeg0 <- unlist(lapply(sim_results, 
@@ -97,7 +61,7 @@ outdeg4 <- unlist(lapply(sim_results,
 
 
 c(mean(outdeg0), mean(outdeg1), mean(outdeg2), mean(outdeg3), mean(outdeg4))
-outedges$n_nodes[1:5]
+outedges$n_nodes[1:5] #target
 
 ## indegree
 indeg0 <- unlist(lapply(sim_results, 
